fc75f2aad528f1bd27d78ab1ecf9ac9ea1eefb7d
Julian Hernando Henao Quintero <jhenaoqu@contratista.epm.co>
2021-10-13 14:37:02 -0500

    adicionando gitignore y eliminando carpeta target

--- target/test-classes/soap/consultas/ntlm-auth.js
+++
@@ -1,5 +0,0 @@
-function fn(creds) {
-  var temp = creds.username + ':' + creds.password;
-  var res = karate.exec('curl POST '+ creds.url + '-v --ntlm --negotiate -u'+  creds.username + ':' + creds.password)
-  return res;
-}
